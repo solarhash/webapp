e04686b2ccf68597654d721c25d08be7ba274882
Chance Hudson <jchancehud@gmail.com>
2021-02-22 00:11:15 -0600

    wip

--- src/Home.vue
+++ src/Home.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-    Hello World
+    <div class="header">solarhash</div>
+    <div class="container">
+      <div>
+        <input type="text" v-model="hash" v-on:change="hashChanged" />
+        <button v-on:click="generateHash">Generate Hash</button>
+      </div>
+      <div style="font-family: monospace">
+        <p style="white-space:pre-wrap">{{ hashart }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
 import Component from 'vue-class-component'
+import solarhash from 'solarhash'
 
 @Component({
   name: 'Home',
@@ -16,6 +26,27 @@ import Component from 'vue-class-component'
   },
 })
 export default class Home extends Vue {
+  hash = ''
+  hashart = ''
+
+  hashChanged() {
+    console.log('updated!', this.hash)
+    this.updateArt()
+  }
+
+  generateHash() {
+    let hash = '0x'
+    for (let x = 0; x < 40; x++) {
+      hash += (Math.floor(Math.random() * 16)).toString(16)
+    }
+    this.hash = hash
+    this.updateArt()
+  }
+
+  updateArt() {
+    this.hashart = solarhash(this.hash)
+  }
+
   async serverPrefetch() {
     // probably calculate a hash here
   }
@@ -26,4 +57,10 @@ export default class Home extends Vue {
 </script>
 
 <style scoped>
+.header {
+  font-size: 30px;
+}
+.container {
+  display: flex;
+}
 </style>
